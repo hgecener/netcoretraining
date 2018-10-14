2c12c7a7578814a774fe053496dcdaaf1637cf74
Hakan Gecener <hg2008email-git@yahoo.com>
2018-10-14 15:45:21 -0400

    Add

--- Startup_AppUse1.cs
+++ Startup_AppUse1.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
-    public class Startup
+    public class StartupAppUse1
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
