f5921b1f2d96216af7e4269816b974ebda0a0e19
Hakan Gecener <hg2008email-git@yahoo.com>
2018-11-24 16:05:55 -0500

    Razor Pages - Model

--- Pages/Greeting.cshtml.cs
+++ Pages/Greeting.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
 
 namespace OdeToFood.Pages
 {
+    // Greeting.cshtml implement using @model
     public class GreetingModel : PageModel
     {
+        private IGreeter _greeter;
+        public string CurrentGreeting {get; set;}
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
         public void OnGet()
         {
+            CurrentGreeting = _greeter.GetMessageOfTheDay();
         }
     }
 }
